e5122f330442d08ee00d37c1333534ebf26ff7b3
dundorma <90293691+dundorma@users.noreply.github.com>
2025-01-25 13:33:51 +0700

    feat: add permission, signing, selinux, and much more

---
+++ Android/android-sdk/devshell.nix
@@ -0,0 +1,29 @@
+{ pkgs }:
+
+with pkgs;
+devshell.mkShell {
+  name = "android-project";
+  motd = ''
+    Entered the Android app development environment.
+  '';
+  env = [
+    {
+      name = "ANDROID_HOME";
+      value = "${android-sdk}/share/android-sdk";
+    }
+    {
+      name = "ANDROID_SDK_ROOT";
+      value = "${android-sdk}/share/android-sdk";
+    }
+    {
+      name = "JAVA_HOME";
+      value = jdk.home;
+    }
+  ];
+  packages = [
+    android-sdk
+    gradle
+    jdk
+  ];
+}
+
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-
-
 # Android
 
 | No | Topic |
 | -------------- | --------------- |
 | 1 | [Android Account Isolation ](./Android/1.android_account_isolation.md)  |
+| 2 | [Permission](./Android/2.permission.md)  |
+| 3 | [Apk Signing](./Android/3.signing.md)  |
+| 4 | [Android SELinux](./Android/4.android_selinux.md)  |
+| 5 | [Android Encryption](./Android/5.encryption.md)  |
+| 6 | [Android Updates](./Android/6.updates.md)  |
 
